e93036998c019a906580eb11fc783234f1e8da6e
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-09 17:21:21 -0300

    bootstrap: fastify http server

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,7 +1,25 @@
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
+import {
+    hasZodFastifySchemaValidationErrors,
+    serializerCompiler,
+    validatorCompiler,
+} from "fastify-type-provider-zod";
 
 const server = fastify();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.validation,
+    });
+  }
+  return reply.status(500).send({ message: "Internal server error." });
+});
 
 server.register(fastifyCors, { origin: "*" });
 
