04cff69517422e5aaa05757d7c0d2f63fcb17c4e
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-10 05:31:47 -0300

    add: docs path

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,7 +1,10 @@
 import { errorHandler } from "@/shared/error-handler";
 import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { fastify } from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
@@ -14,6 +17,20 @@ server.setSerializerCompiler(serializerCompiler);
 server.setErrorHandler((error, _, reply) => errorHandler(reply, error));
 
 server.register(fastifyCors, { origin: "*" });
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Brevly Server",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP Server running!");
