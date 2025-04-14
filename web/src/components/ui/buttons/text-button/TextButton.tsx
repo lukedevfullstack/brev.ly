092900ebfd73878f6fc4c5d85fc27a2cf4aac8aa
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-14 17:39:45 -0300

    add: core components

---
+++ web/src/components/ui/buttons/text-button/TextButton.tsx
@@ -0,0 +1,45 @@
+import { ComponentSizes } from "@/types/components-sizes";
+import { ComponentVariants } from "@/types/components-variants";
+import { twMerge } from "tailwind-merge";
+
+interface TextButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  children: React.ReactNode;
+  variant?: ComponentVariants;
+  size?: ComponentSizes;
+}
+
+const variantStyle: Record<ComponentVariants, string> = {
+  primary:
+    "bg-[var(--blue-base)] text-white enabled:hover:bg-[var(--blue-dark)]",
+  secondary:
+    "bg-[var(--gray-200)] text-gray-500 enabled:hover:bg-[var(--gray-300)]",
+};
+
+const sizeStyle: Record<ComponentSizes, string> = {
+  small: "text-sm",
+  medium: "text-md",
+  large: "text-lg",
+};
+
+export const TextButton = ({
+  className,
+  variant = "primary",
+  size = "medium",
+  children,
+  ...props
+}: TextButton) => {
+  return (
+    <button
+      className={twMerge(
+        "rounded-base flex h-12 w-[19.75rem] items-center justify-center gap-[0.375rem] transition-all duration-150 enabled:cursor-pointer disabled:opacity-50",
+        variantStyle[variant],
+        sizeStyle[size],
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
