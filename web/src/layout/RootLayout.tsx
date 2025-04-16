658351879a2a3b9bcee3d088d9e951957d4ffd6e
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-16 20:42:36 -0300

    update: minor layout changes

--- web/src/layout/RootLayout.tsx
+++ web/src/layout/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export const RootLayout = () => {
   return (
-    <section className="h-dvh w-screen flex-1 flex-col items-start justify-start">
+    <section className="h-dvh w-screen flex">
       <Outlet />
     </section>
   );
