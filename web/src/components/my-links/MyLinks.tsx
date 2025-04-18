c26f990b7f3022a6c031016bc609272e85e76cca
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-18 13:32:15 -0300

    add: downloadcsv component & utility functions for download

--- web/src/components/my-links/MyLinks.tsx
+++ web/src/components/my-links/MyLinks.tsx
@@ -1,8 +1,8 @@
 import { Icons } from "@/icons/Icons";
 import { Link } from "@/types/link";
-import { TextButton } from "../ui/buttons/text-button/TextButton";
 import { Divider } from "../ui/divider/Divider";
 import { ScrollArea } from "../ui/scroll-area/ScrollArea";
+import { DownloadCSV } from "./download-csv/DownloadCsv";
 import { LinkCard } from "./link-card/LinkCard";
 
 interface MyLinks {
@@ -10,42 +10,39 @@ interface MyLinks {
   loading: boolean;
 }
 
+const NoRegisteredLinks = () => (
+  <span className="flex h-auto w-auto flex-1 flex-grow-0 flex-col items-center justify-center gap-4 self-stretch px-4 py-6">
+    <Icons.Link className="text-[var(--gray-400)]" />
+    <p className="text-xs text-[var(--gray-500)] uppercase">
+      Ainda não existem links cadastrados
+    </p>
+  </span>
+);
+
 export const MyLinks = ({ links }: MyLinks) => {
   return (
     <div className="3xl:col-span-6 3xl:w-auto 3xl:flex-1 3xl:min-w-[36.25rem] 3xl:gap-6 3xl:h-auto 3xl:p-8 relative flex h-[21.75rem] w-[22.875rem] flex-col items-start gap-[1.25rem] rounded-lg bg-[var(--gray-100)] p-6">
       <span className="flex w-auto flex-1 items-center justify-between self-stretch">
         <h2 className="text-lg">Meus links</h2>
-        <TextButton variant="secondary" size="small">
-          <Icons.DownloadSimple className="size-4" />
-          <span>Baixar CSV</span>
-        </TextButton>
+        <DownloadCSV />
       </span>
       <div className="flex w-full flex-col">
         <Divider />
         {links.length > 0 ? (
           <ScrollArea className="divide-y-[1px] divide-[var(--gray-200)]">
-            {links?.length > 0 ? (
-              links.map((link) => (
-                <li key={link.id}>
-                  <LinkCard
-                    id={link.id}
-                    originalUrl={link.originalUrl}
-                    shortUrl={link.shortUrl}
-                    visitCount={link.visitCount}
-                  />
-                </li>
-              ))
-            ) : (
-              <></>
-            )}
+            {links.map((link) => (
+              <li key={link.id}>
+                <LinkCard
+                  id={link.id}
+                  originalUrl={link.originalUrl}
+                  shortUrl={link.shortUrl}
+                  visitCount={link.visitCount}
+                />
+              </li>
+            ))}
           </ScrollArea>
         ) : (
-          <span className="flex h-auto w-auto flex-1 flex-grow-0 flex-col items-center justify-center gap-4 self-stretch px-4 py-6">
-            <Icons.Link className="text-[var(--gray-400)]" />
-            <p className="text-xs text-[var(--gray-500)] uppercase">
-              Ainda não existem links cadastrados
-            </p>
-          </span>
+          <NoRegisteredLinks />
         )}
       </div>
     </div>
---
+++ web/src/components/my-links/download-csv/DownloadCsv.tsx
@@ -0,0 +1,65 @@
+import { TextButton } from "@/components/ui/buttons/text-button/TextButton";
+import { Dialog } from "@/components/ui/dialog/Dialog";
+import { useRequest } from "@/hooks/use-request";
+import { useToggle } from "@/hooks/use-toggle";
+import { Icons } from "@/icons/Icons";
+import { getFilenameFromUrl } from "@/utils/get-filename-from-url";
+import { triggerDownloadFromBlob } from "@/utils/trigger-download-from-blob";
+
+export const DownloadCSV = () => {
+  const [isErrorDialogToggled, onErrorDialogToggle] = useToggle();
+  const getReportUrl = useRequest<{ reportUrl: string }, string>();
+  const downloadCSV = useRequest();
+
+  const isLoading = getReportUrl.loading || downloadCSV.loading;
+
+  return (
+    <>
+      <TextButton
+        variant="secondary"
+        size="small"
+        active={isLoading}
+        disabled={isLoading}
+        onClick={() =>
+          getReportUrl.sendRequest("/api/urls/exports", {
+            method: "POST",
+            body: {},
+            onSuccess: ({ reportUrl }) => {
+              if (!reportUrl) return;
+              downloadCSV.sendRequest(reportUrl, {
+                method: "GET",
+                mode: "cors",
+                onSuccess: async (_, response: Response) => {
+                  try {
+                    const blob = await response.blob();
+                    const filename = getFilenameFromUrl(reportUrl);
+                    triggerDownloadFromBlob(blob, filename);
+                  } catch (err) {
+                    onErrorDialogToggle();
+                    console.error("Failed to download CSV:", err);
+                  }
+                },
+              });
+            },
+          })
+        }
+      >
+        {isLoading ? (
+          <Icons.Loading className="animate-fast-spin size-4" />
+        ) : (
+          <Icons.DownloadSimple className="size-4" />
+        )}
+        <span>Baixar CSV</span>
+      </TextButton>
+      <Dialog
+        isOpen={isErrorDialogToggled}
+        onClose={onErrorDialogToggle}
+        variant="error"
+        title="Erro"
+        description="Erro ao baixar CSV. Tente novamente mais tarde."
+        className="divide-red-200 bg-red-50 text-red-600"
+        selfClosesAfter={5000}
+      />
+    </>
+  );
+};
