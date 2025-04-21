09e17fd12df6ee84312ea39605e3c039623362df
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-04-21 11:09:23 -0300

    add: link create feature

--- web/src/components/new-link/NewLink.tsx
+++ web/src/components/new-link/NewLink.tsx
@@ -1,27 +1,74 @@
+import { useForm } from "@/hooks/use-form";
 import { Icons } from "@/icons/Icons";
+import { validateLongUrl } from "@/utils/validate-long-url";
+import { validateShortUrl } from "@/utils/validate-short-url";
 import { Button } from "../ui/buttons/button/Button";
 import { TextInput } from "../ui/inputs/text-input/TextInput";
 import { Label } from "../ui/label/Label";
 
-export const NewLink = () => {
+interface NewLink {
+  onLinkCreate: (params: { originalUrl: string; shortUrl: string }) => void;
+}
+
+const formFields = [
+  {
+    key: "originalUrl",
+    label: "link original",
+    placeholder: "www.exemplo.com.br",
+    prefix: "",
+  },
+  {
+    key: "shortUrl",
+    label: "link encurtado",
+    placeholder: "",
+    prefix: "Brev.ly/",
+  },
+] as const;
+
+export const NewLink = ({ onLinkCreate }: NewLink) => {
+  const { values, errors, submitting, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      originalUrl: "",
+      shortUrl: "",
+    },
+    validators: {
+      originalUrl: validateLongUrl,
+      shortUrl: validateShortUrl,
+    },
+    onSubmit: onLinkCreate,
+  });
+
   return (
-    <div className="3xl:col-span-4 3xl:w-auto 3xl:flex-1 3xl:max-w-[23.75rem] 3xl:h-[21.25rem] 3xl:p-8 relative flex h-[19.75rem] max-h-fit w-[22.875rem] flex-1 flex-col items-start gap-6 self-stretch rounded-lg bg-[var(--gray-100)] p-6">
+    <div className="3xl:col-span-4 3xl:w-auto 3xl:flex-1 3xl:max-w-[23.75rem] 3xl:min-h-[21.25rem] 3xl:p-8 relative flex h-fit min-h-[19.75rem] w-[22.875rem] flex-1 flex-col items-start gap-6 self-stretch rounded-lg bg-[var(--gray-100)] p-6">
       <Icons.Logo className="max-3xl:right-0 max-3xl:w-full 3xl:-top-[3.5rem] absolute -top-[3rem] left-0 text-[var(--blue-dark)] duration-150" />
       <h2 className="text-lg">Novo link</h2>
-      <div className="flex flex-col gap-4">
-        <Label label="link original">
-          <TextInput placeholder="www.exemplo.com.br" />
-        </Label>
-        <Label label="link encurtado">
-          <div className="relative">
-            <TextInput className="flex justify-start pl-[3.625rem]" />
-            <span className="absolute top-4 left-[1.025rem] font-normal text-[var(--gray-400)] lowercase">
-              Brev.ly/
-            </span>
+
+      <div className="flex w-full flex-col gap-4">
+        {formFields.map(({ key, label, placeholder, prefix }) => (
+          <div key={key} className="relative w-full">
+            <Label label={label} error={errors[key]}>
+              <TextInput
+                id={key}
+                placeholder={placeholder}
+                className={prefix ? "pl-[3.625rem]" : ""}
+                value={values[key]}
+                hasError={!!errors[key]}
+                onChange={(e) => handleChange(key, e.target.value)}
+              />
+            </Label>
+
+            {prefix && (
+              <span className="absolute top-10 left-[1.025rem] text-sm font-normal text-[var(--gray-400)] lowercase">
+                {prefix}
+              </span>
+            )}
           </div>
-        </Label>
+        ))}
       </div>
-      <Button>Salvar link</Button>
+
+      <Button onClick={handleSubmit} disabled={submitting}>
+        {submitting ? "Salvando..." : "Salvar link"}
+      </Button>
     </div>
   );
 };
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { useToast } from "@/components/ui/toast/toast-context/ToastContext";
 import { useOnMount } from "@/hooks/use-on-mount";
 import { useRequest } from "@/hooks/use-request";
 import { Link } from "@/types/link";
+import { normalizeUrl } from "@/utils/normalize-url";
 import { useState } from "react";
 
 export const Home = () => {
@@ -11,6 +12,10 @@ export const Home = () => {
   const { loading, sendRequest: fetchAllLinks } = useRequest<
     { urls: Link[] },
     string
+  >();
+  const { sendRequest: createLink } = useRequest<
+    Link,
+    { originalUrl: string; shortUrl: string }
   >();
   const { sendRequest: fetchSingleLink } = useRequest<Link, string>();
   const { sendRequest: deleteLink } = useRequest<null, string>();
@@ -26,7 +31,41 @@ export const Home = () => {
   return (
     <main className="page 3xl:justify-center items-center">
       <div className="3xl:grid 3xl:grid-cols-10 3xl:gap-[1.25rem] 3xl:w-full 3xl:px-[15%] 3xl:pt-0 relative flex flex-col gap-3 pt-20">
-        <NewLink />
+        <NewLink
+          onLinkCreate={(data: { originalUrl: string; shortUrl: string }) => {
+            const normalizedUrl = normalizeUrl(data.originalUrl);
+
+            createLink("/api/urls", {
+              method: "POST",
+              body: {
+                originalUrl: normalizedUrl,
+                shortUrl: data.shortUrl,
+              },
+              onSuccess: (newLink) => {
+                pushToast({
+                  variant: "success",
+                  title: "Sucesso!",
+                  description: `O link "${newLink.shortUrl}" foi criado com sucesso!`,
+                  className: "text-[var(--blue-base)]",
+                });
+
+                setLinks((prev) => [newLink, ...prev]);
+              },
+              onError: (err, res) => {
+                console.warn(err);
+                pushToast({
+                  variant: "error",
+                  title: "Erro",
+                  description:
+                    res?.status === 409
+                      ? "Link encurtado já existe."
+                      : "Erro ao criar o link. Tente novamente mais tarde.",
+                  className: "text-red-600",
+                });
+              },
+            });
+          }}
+        />
         <MyLinks
           links={links}
           loading={loading}
